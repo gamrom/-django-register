f48d35e0dafd6acca4587d3eef8669cc32642745
gamrom <gamrom01@naver.com>
2019-08-16 11:16:27 +0900

    부트스트랩 테마 바꾸기

--- fcuser/views.py
+++ fcuser/views.py
@@ -9,13 +9,14 @@ def register(request):
         return render(request, 'register.html') #파일 위치는 템플릿 폴더를 자동적으로 찾는다. 만약 폴더안에 폴더면 폴더경로를 써주면됨.
     elif request.method == "POST":
         username = request.POST.get('username', None) #예외 처리하기위해서 name으로 받은 값들을 기본적으로 None으로 설정
+        useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
         re_password = request.POST.get('re-password', None)
 
         res_data = {}
 
         #예외처리
-        if not (username and password and re_password): #세개의 input중 적어도 한 개가 비었다면,
+        if not (username and password and re_password and useremail): #세개의 input중 적어도 한 개가 비었다면,
             res_data['error'] = '모든 값을 입력해야합니다'
 
         elif password != re_password: #비밀번호 두개가 서로 일치하는지 확인
@@ -25,6 +26,7 @@ def register(request):
         else:
             fcuser = Fcuser(
                 username=username,
+                useremail=useremail,
                 password=make_password(password) #비밀번호 암호화 저장
             )
             fcuser.save()
